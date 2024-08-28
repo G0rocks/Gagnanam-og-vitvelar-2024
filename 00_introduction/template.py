bdb946798a409f0567ac7293b28556271890b0bc
G0rocks <huldarh@gmail.com>
2024-08-28 11:38:55 +0000

    Assignment 00 and starting assignment 01

--- 00_introduction/template.py
+++ 00_introduction/template.py
@@ -1,27 +1,120 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(0)   # To have the same as gradescope
 
-def normal(x: np.ndarray, sigma: np.float64, mu: np.float64) -> np.ndarray:
+def normal(x: np.ndarray, sigma: float, mu: float) -> np.ndarray:
     # Part 1.1
+    p = np.exp(-(np.power((x - mu),2))/(2*np.power(sigma,2)) )/np.sqrt(2*np.pi*np.power(sigma,2))
+    return p
 
-def plot_normal(sigma: np.float64, mu:np.float64, x_start: np.float64, x_end: np.float64):
+def plot_normal(sigma: float, mu:float, x_start: float, x_end: float):
     # Part 1.2
+    x_range = np.linspace(x_start, x_end, 500)
+    plt.plot(x_range, normal(x_range,sigma, mu))
 
 def _plot_three_normals():
     # Part 1.2
+    plt.clf()
+    mu = 0
+    sigma = 0.5
+    plot_normal(sigma,mu,-5,5)
+    mu = 1
+    sigma = 0.25
+    plot_normal(sigma,mu,-5,5)
+    mu = 1.5
+    sigma = 1
+    plot_normal(sigma,mu,-5,5)
+
+    plt.savefig("./1_2_1.png")
+
 
 def normal_mixture(x: np.ndarray, sigmas: list, mus: list, weights: list):
     # Part 2.1
+    sum = 0
+    for i in range(len(weights)):
+        sum = sum + weights[i]*np.exp(-np.power(x-mus[i],2)/(2*np.power(sigmas[i],2)))/(np.sqrt(2*np.pi*np.power(sigmas[i],2)))
+
+    return sum
 
 def _compare_components_and_mixture():
     # Part 2.2
+    plt.clf()
+    mus = [0, -0.5, 1.5]
+    sigmas = [0.5, 1.5, 0.25]
+    phis = [1/3, 1/3, 1/3]
 
-def sample_gaussian_mixture(sigmas: list, mus: list, weights: list, n_samples: int = 500):
+    for i in range(len(mus)):
+        plot_normal(sigmas[i],mus[i],-5,5)
+
+    x_range = np.linspace(-5, 5, 500)
+    plt.plot(x_range, normal_mixture(x_range,sigmas, mus, phis))
+
+    plt.savefig("./2_2_1.png")
+    plt.clf()
+
+def sample_gaussian_mixture(sigmas: list, mus: list, weights: list, n_samples: int = 500) -> np.ndarray:
     # Part 3.1
+    print("\nPart 3.1")
 
-def _plot_mixture_and_samples():
+    # Share the number of samples between each sigma (each normal component)
+    normal_components_n_samples = np.random.multinomial(n_samples, weights)
+    
+    # Initialize empty list of samples
+    samples_list = []
+
+    # For each sigma, get an array of samples until we have the correct amount of samples for that sigma, then move on to sample from next sigma. Append all samples to samples_list
+    for i in range(len(sigmas)):
+        for j in range(normal_components_n_samples[i]): #range(len(sigmas)):
+            samples_list.append(np.random.normal(mus[i], sigmas[i]))
+
+    return np.array(samples_list)
+
+#def _plot_mixture_and_samples():
     # Part 3.2
 
 if __name__ == '__main__':
     # select your function to test here and do `python3 template.py`
+    # Part 1.1
+    #print("Test 1:")
+    #print(normal(0, 1, 0) == 0.3989422804014327)
+    #print("Test 2:")
+    #print(normal(3, 1, 5) == 0.05399096651318806)
+    #print("Test 3:")
+    #print(normal(np.array([-1,0,1]), 1, 0))
+    #print(np.array[0.24197072, 0.39894228, 0.24197072])
+    #print(normal(np.array([-1,0,1]), 1, 0) == [0.24197072, 0.39894228, 0.24197072])
+
+    # Part 1.2
+    #_plot_three_normals()
+
+    # Part 2.1
+    #print("Part 2.1")
+    #print("example 1")
+    #print(normal_mixture(np.linspace(-5, 5, 5), [0.5, 0.25, 1], [0, 1, 1.5], [1/3, 1/3, 1/3]))
+    #print(normal_mixture(np.linspace(-2, 2, 4), [0.5], [0], [1]))
+
+    #_compare_components_and_mixture()
+
+    # Part 3.1
+    '''
+    results = sample_gaussian_mixture(sigmas=[0.1, 1], mus=[-1, 1], weights=[0.9, 0.1], n_samples=3)
+    if (results[0] == -0.92584083 and results[1] == -0.84470863 and results[2] == -1.22683282):
+        print("Pass")
+    else:
+        print("Fail")
+    '''
+    #print(sample_gaussian_mixture([0.1, 1, 1.5], [1, -1, 5], [0.1, 0.1, 0.8], 10))
+    
+    # Part 3.2
+    
+    
+    # Confirmation message for a succesful run
+    print("\n---------------------------------------------------------------\nRun succesful :)\n")
+    
+'''
+if == :
+    print("Pass")
+else:
+    print("Fail")
+'''
--- 01_sequential_estimation/template.py
+++ 01_sequential_estimation/template.py
@@ -1,6 +1,6 @@
-# Author: 
-# Date:
-# Project: 
+# Author: Huldar
+# Date: 2024-08-28
+# Project: Assignment 1
 # Acknowledgements: 
 #
 
@@ -9,6 +9,8 @@ import numpy as np
 
 from tools import scatter_2d_data, bar_per_axis
 
+# Use same random seed as professor
+np.random.seed(1234)
 
 def gen_data(
     n: int,
@@ -16,10 +18,23 @@ def gen_data(
     mean: np.ndarray,
     var: np.float64
 ) -> np.ndarray:
-    '''Generate n values samples from the k-variate
-    normal distribution
     '''
-    pass
+    Generate n values samples from the k-variate
+    normal distribution.
+    n: Number of vectors
+    k: Dimension of each vector
+    mean:   Mean for K-variate normal distribution
+    var:    variance for K-variate normal distribution
+
+    Returns nxK array X of vectors x_i
+    '''
+    # Create variance array (no covariance)
+    variance_matrix = np.identity(k)*var*var
+
+    X_array = np.random.multivariate_normal(mean, variance_matrix, size=n)
+
+    # Return X_array
+    return X_array
 
 
 def update_sequence_mean(
@@ -73,8 +88,222 @@ def _plot_changing_sequence_estimate():
     pass
 
 
-if __name__ == "__main__":
-    """
-    Keep all your test code here or in another file.
-    """
+
+# Test area
+# -----------------------------------------------------
+if __name__ == '__main__':
+    # Part 1.1
+    print("Part 1.1\n is:")
+    print(gen_data(2, 3, np.array([0, 1, -1]), 1.3))
+    print("\n The same as\n")
+    print(np.array([[ 0.61286571, -0.5482684 ,  0.86251906],
+                    [-0.40644746,  0.06323465,  0.15331182]]))
+    print("\n And is:\n")
+    print(gen_data(5, 1, np.array([0.5]), 0.5))
+    print("\n The same as\n")
+    print(np.array([[ 0.73571758],
+                    [-0.09548785],
+                    [ 1.21635348],
+                    [ 0.34367405],
+                    [ 0.13970563]]))
+
+
+    # Part 1.2
+    print("Part 1.2")
+    # Create 300 3-dimensional data points sampled, write to file
+    data_1_2 = gen_data(300, 3, [0,1,-1], np.sqrt(3))
+    # Plot 3D data
+    #scatter_3d_data(data_1_2)
+    #bar_per_axis(data_1_2)
+
+    # Answer to written question
+    text_answer = "Do you expect the batch estimate to be exactly (0,1,-1)?\nYes. I do, because that's what we generated it as.\n\nWhich two parameters can be used to make this estimate more accurate? I have no clue, really don't understand the statistics I'm doing right now\nhttps://m.media-amazon.com/images/I/41sKf2ToyPL.jpg"
+    with open('.\\03_sequential_estimation\\1_2_1.txt', 'w') as f:
+        f.write(str(text_answer))
+
+    # Part 1.3
+    print("Part 1.3")
+
+    
+    # Part 1.4
+    print("Part 1.4")
+    X = data_1_2
+    mean = np.mean(X, 0)
+    new_x = gen_data(1, 3, np.array([0, 0, 0]), 1)
+    print(update_sequence_mean(mean, new_x, X.shape[0]))
+
+
+    # Part 1.5
+    print("Part 1.5")
+    # Create 100 3-dimensional data points with mean [0,0,0] and variance 4
+    mean = [0,0,0]
+    variance = 4
+    data_1_5 = gen_data(100, 3, mean, variance)
+    _plot_sequence_estimate(data_1_5, save_fig=".\\03_sequential_estimation\\1_5_1.png")
+    #_plot_sequence_estimate(data_1_5)
+
+
+    # Part 1.6
+    print("Part 1.6")
+    #_plot_mean_square_error(data_1_5, mean)
+    _plot_mean_square_error(data_1_5, mean, ".\\03_sequential_estimation\\1_6_1.png")
+
+
+    # Confirmation message for a succesful run
+    print("\n---------------------------------------------------------------\nRun succesful :)\n")
+
+
+
+'''
+    if == :
+        print("Pass")
+    else:
+        print("Fail")
+'''
+
+# From 2023
+#-----------------------------------------------------
+# Author:   Huldar
+# Date:     2023-09-12
+# Project:  Assignment 3
+# Acknowledgements: 
+#
+
+
+from tools import scatter_3d_data, bar_per_axis
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+def update_sequence_mean(
+    mu: np.ndarray,
+    x: np.ndarray,
+    n: int
+) -> np.ndarray:
+    '''
+    Performs the mean sequence estimation update.
+    mu: Mean
+    x:  1 Vector from X_array
+    n:  Number of vectors in X_array
+
+    Returns a mean estimation for the whole matrix???
+    '''
+    return mu + (x-mu)/n
+
+
+def _plot_sequence_estimate(data, save_fig=None):
+    '''
+    Plots a sequence estimate for all the vectors in the data with an initial estimate of zero with k-dimensions
+    data:   n x k array with an n number of k-dimensional vectors
+    save_fig:   "Name of file to save figure into, if no file name is given, will show plot and not save. Default value is None.
+    '''
+    # Initialize inital estimate as zero array with k-dimensions
+    k = data.shape[1]
+    estimates = [np.zeros(k)]   # Note, this is a list of mean estimation vectors
+
+    # For each vector in the data matrix, get an updated_sequence_mean. Collect estimates as we go
+    for i in range(data.shape[0]):
+        # Add new estimate to collection
+        estimates.append(update_sequence_mean(estimates[i], data[i], i+1))
+
+    # Generate plot from estimation for each dimension
+    for i in range(k):
+        plt.plot([e[i] for e in estimates], label=(str(i+1)+' dimension'))
+
+    # Title plot
+    plt.title("Mean estimates per data point")
+    # Label axis
+    plt.xlabel("Data points used for estimation") # Add ", fontsize = #" to control fontsize
+    plt.ylabel("Mean estimation")
+    plt.legend(loc='upper center')
+
+    if save_fig == None:
+        # Show plot
+        plt.show()
+        return
+    
+    plt.savefig(save_fig)
+
+
+def _square_error(y, y_hat):
+    '''
+    Finds the square error between estimation and actual mean after every update.
+    y:      Actual mean
+    y_hat:  Estimation
+
+    returns error averaged across all dimensions (average error)
+    '''
+    # Calculate error for each dimension
+    error = np.power(y-y_hat,2)
+    # Return average error
+    return np.average(error)
+
+def _plot_mean_square_error(data, mean, save_fig=None):
+    '''
+    Finds the square error between estimation and actual mean after every update.
+    Initial estimation is a zero vector.
+    data:       n x k array with an n number of k-dimensional vectors
+    mean:       k-dimensional mean vector
+    save_fig:   "Name of file to save figure into, if no file name is given, will show plot and not save. Default value is None.
+    '''
+    # Initialize inital estimate as zero array with k-dimensions
+    k = data.shape[1]   # Dimensions
+    estimates = [np.zeros(k)]   # Note, this is a list of mean estimation vectors
+
+    # For each vector in the data matrix, get an updated_sequence_mean. Collect estimates as we go
+    for i in range(data.shape[0]):
+        # Add new estimate to collection
+        estimates.append(update_sequence_mean(estimates[i], data[i], i+1))
+
+    # Initialize empty error_list
+    error_list = []
+
+    # For each estimation, find error between true mean and estimated mean
+    for estimate in estimates:
+        error_list.append(_square_error(mean, estimate))
+
+    # Clear figure
+    plt.clf()
+
+    # Plot error over each estimate:
+    plt.plot(error_list)
+    
+    # Title plot
+    plt.title("Average error between estimate and mean per data point")
+    # Label axis
+    plt.xlabel("Data points used for estimation") # Add ", fontsize = #" to control fontsize
+    plt.ylabel("Error")
+
+    # Plot show/save
+    if save_fig == None:
+        # Show plot
+        plt.show()
+        return
+    
+    plt.savefig(save_fig)
+
+# Naive solution to the independent question.
+
+def gen_changing_data(
+    n: int,
+    k: int,
+    start_mean: np.ndarray,
+    end_mean: np.ndarray,
+    var: float
+) -> np.ndarray:
+    # remove this if you don't go for the independent section
     pass
+
+
+def _plot_changing_sequence_estimate():
+    # remove this if you don't go for the independent section
+    pass
+
+
+
+
+

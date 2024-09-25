f7a7960a426fe4876f303045753621e0f7eac0af
G0rocks <huldarh@gmail.com>
2024-09-25 10:52:57 +0000

    Part 5

--- 03_linear_regression/template.py
+++ 03_linear_regression/template.py
@@ -55,11 +55,10 @@ def mvn_basis(
         # Get the current mean vector
         mean_vec = mu[i].numpy()
         
-        # Calculate the multivariate normal distribution for the current mean
-        # and the isotropic covariance matrix
+        # Calculate the multivariate normal distribution for the current mean and the isotropic covariance matrix
         mvn = multivariate_normal(mean=mean_vec, cov=cov_matrix)
         
-        # Compute the PDF for all features (NxD) and store it in the ith column of design_matrix
+        # Compute the PDF () for all features (NxD) and store it in the ith column of design_matrix
         design_matrix[:, i] = torch.tensor(mvn.pdf(features.numpy()))
     
     # design_matrix = multivariate_normal.pdf(features, mean_vec, cov_matrix) # NOT WORKING BECAUSE OF NUMPY ERROR!!!
@@ -68,12 +67,19 @@ def mvn_basis(
     # Return phi
     return design_matrix
 
-def _plot_mvn():
+def _plot_mvn(design_matrix):
     '''
     Plot the output of each basis function, using the same parameters as above, as a function of the features.
     You should plot all the outputs onto the same plot.
     Turn in your plot as 2_1.png.
+    
+    Input:
+    design_matrix: (N x M) capital phi matrix
     '''
+    # Find M and M
+    N = design_matrix.shape[0]
+    M = design_matrix.shape[1]
+    
     # Loop through each basis function
     for i in range(M):
         # Plot basis function as a result of features, design_matrix output on Y-axis and feature ID on X-axis
@@ -105,11 +111,15 @@ def max_likelihood_linreg(
     * fi: [NxM] is the array of basis function vectors - THIS IS THE DESIGN MATRIX FROM EQ. (4.15) IN BISHOP!!!
     * t: [Nx1] is the target value for each input in Fi
     * lamda: The regularization constant --> regularization matrix (lamda*I) [MxM]
+    * M:    Added by Huldar. Seems that the gradescope gives me an error for not having it so I added it.
 
     Output: [Mx1], the maximum likelihood estimate of w for the linear model, w_ML
     '''
     # We have the design matrix (I call it a basis function matrix) - Eq. (4.15) from Bishop
     design_matrix = fi
+    
+    # Find M
+    M = design_matrix.shape[1]
     
     # Find Moore-Penrose pseudo-inverse (MPp_inverse) of design matrix with regularization coefficient - Eq. (4.14) modified to Eq. (4.27) from Bishop
     # We can't use this since we have the lambda
@@ -133,8 +143,9 @@ def max_likelihood_linreg(
     design_matrix_T = torch.transpose(design_matrix,0,1)
     
     # Finding maximum likelihood for weights (w_ML) - Eq. (4.27) from Bishop
-    w_ML = torch.matmul(torch.inverse(reg_matrix + torch.matmul(design_matrix_T, design_matrix)), design_matrix_T)*targets
-    
+    # w_ML = torch.matmul(torch.inverse(reg_matrix + torch.matmul(design_matrix_T, design_matrix)), design_matrix_T)*targets
+    w_ML = (torch.inverse(reg_matrix + design_matrix_T@design_matrix)@design_matrix_T)*targets
+        
     # return weights
     return w_ML
 
@@ -173,12 +184,13 @@ def linear_model(
     # Prediction of regression values with given weights - Eq. (4.29) in Bishop - Note: Switch order of matrix multiplication since it's equal to transposing both of them
     # Note, had a hard time implementing equations with torch.matmul(w, design_matrix) or torch.matmul(design_matrix, w) so ended up doing by hand
     # Loop through each data point and make a linear combination of the design matrix values and the weights
-    for i in range(N):
+#    for i in range(N):
         # Prediction for datapoint i, dot product between weight column i and design_matrix row i
-        predictions[i] = torch.dot(w[:,i],design_matrix[i,:])
+        # predictions[i] = torch.dot(w[:,i],design_matrix[i,:])
+    predictions = design_matrix@w
         
     # Return prediction:
-    return predictions[:,0]
+    return predictions
 
 def _square_error(actual: torch.Tensor, est: torch.Tensor) -> torch.Tensor:
     '''
@@ -226,7 +238,7 @@ if __name__ == "__main__":
     # Part 2
     print("Part 2")
     print("Plotting...")
-    _plot_mvn()
+    _plot_mvn(design_matrix)
     print("Plot finished :)")
 
     # Part 3
